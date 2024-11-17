062a0b86450f50dbb87009331a17d1f8b886b298
Nelson Estevão <d12733@di.uminho.pt>
2024-11-17 21:53:13 +0000

    🗼

---
+++ lib/Tarefa1.hs
@@ -0,0 +1,15 @@
+{-|
+Module      : Tarefa1
+Description : Invariantes do Jogo
+Copyright   : Matheus Henrique Monteiro da Silva Azevedo <a111430@alunos.uminho.pt>
+              Francisco Luciano Martins <a111775@alunos.uminho.pt>
+
+
+Módulo para a realização da Tarefa 1 de LI1 em 2024/25.
+-}
+module Tarefa1 where
+
+import LI12425
+
+validaJogo :: Jogo -> Bool
+validaJogo = undefined
---
+++ lib/Tarefa3.hs
@@ -0,0 +1,15 @@
+{-|
+Module      : Tarefa3
+Description : Mecânica do Jogo
+Copyright   : Matheus Henrique Monteiro da Silva Azevedo <a111430@alunos.uminho.pt>
+              Francisco Luciano Martins <a111775@alunos.uminho.pt>
+
+
+Módulo para a realização da Tarefa 3 de LI1 em 2024/25.
+-}
+module Tarefa3 where
+
+import LI12425
+
+atualizaJogo :: Tempo -> Jogo -> Jogo
+atualizaJogo = undefined
